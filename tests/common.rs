11b3ae3c7653752767d5293c6f9de9fc39626399
Ivan Reutov <nujievik@gmail.com>
2025-12-07 19:14:37 +0500

    Fix tests on macOS

--- tests/common.rs
+++ tests/common.rs
@@ -2,12 +2,20 @@ use fb2_clean::*;
 use std::path::{Path, PathBuf};
 use clap::Parser;
 
+#[cfg(not(target_os = "macos"))]
 pub static ITERABLE: [(InputFileType, &str); 5] = [
     (InputFileType::Fb2Zip, "book.fb2.zip"),
     (InputFileType::Fb2, "dummy.fb2"),
     (InputFileType::Fb2Zip, "dummy.fb2.zip"),
     (InputFileType::Fb2, "dummy.FB2"),
     (InputFileType::Fb2Zip, "dummy.FB2.ZIP"),
+];
+
+#[cfg(target_os = "macos")]
+pub static ITERABLE: [(InputFileType, &str); 3] = [
+    (InputFileType::Fb2Zip, "book.fb2.zip"),
+    (InputFileType::Fb2, "dummy.fb2"),
+    (InputFileType::Fb2Zip, "dummy.fb2.zip"),
 ];
 
 pub fn cfg(args: &[&str]) -> Config {
